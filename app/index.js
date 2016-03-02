681c7b436cd162aca2f703538121f17d0dd4f9d0
calebgregory <calebgregory@gmail.com>
2016-03-02 10:35:52 -0600

    Adding express server, test suite, scripts

---
+++ app/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+app.get('/api/users', (req, res) => {
+  return res.status(200).send('ok')
+})
+
+const server = app.listen(app.get('port'), (err) => {
+  if (err) throw err
+  console.log(`(>'')>*<(''<) App listening on http://localhost:${server.address().port}`)
+})
+
+module.exports = app
